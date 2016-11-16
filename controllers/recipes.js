46647cf90f9c531cb12d4305f15ba53d28716c19
Ivan Kosharov <IKosharov@infragistics.com>
2016-11-16 16:27:54 +0200

    adding some tests

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -1,29 +1,11 @@
 var Recipe = require('../models/recipe');
 
-exports.postRecipe = function(req, res) {
+exports.postRecipe = function (req, res) {
   var recipe = new Recipe();
   recipe.title = req.body.title;
   recipe.userId = req.user._id;
 
-  recipe.save(function(err) {
-    if (err)
-      res.send(err);
-
-    res.json({ message: 'Recipe added', data: recipe });
-  });
-};
-
-exports.getRecipes = function(req, res) {
-  Recipe.find({ userId: req.user._id }, function(err, recipes) {
-    if (err)
-      res.send(err);
-
-    res.json(recipes);
-  });
-};
-
-exports.getRecipe = function(req, res) {
-  Recipe.find({ userId: req.user._id, _id: req.params.recipe_id }, function(err, recipe) {
+  recipe.save(function (err) {
     if (err)
       res.send(err);
 
@@ -31,8 +13,26 @@ exports.getRecipe = function(req, res) {
   });
 };
 
-exports.putRecipe = function(req, res) {
-  Recipe.update({ userId: req.user._id, _id: req.params.recipe_id }, { title: req.body.title }, function(err, num, raw) {
+exports.getRecipes = function (req, res) {
+  Recipe.find({ userId: req.user._id }, function (err, recipes) {
+    if (err)
+      res.send(err);
+
+    res.json(recipes);
+  });
+};
+
+exports.getRecipe = function (req, res) {
+  Recipe.find({ userId: req.user._id, _id: req.params.recipe_id }, function (err, recipe) {
+    if (err)
+      res.send(err);
+
+    res.json(recipe);
+  });
+};
+
+exports.putRecipe = function (req, res) {
+  Recipe.update({ userId: req.user._id, _id: req.params.recipe_id }, { title: req.body.title }, function (err, num, raw) {
     if (err)
       res.send(err);
 
@@ -40,11 +40,11 @@ exports.putRecipe = function(req, res) {
   });
 };
 
-exports.deleteRecipe = function(req, res) {
-  Recipe.remove({ userId: req.user._id, _id: req.params.recipe_id }, function(err) {
+exports.deleteRecipe = function (req, res) {
+  Recipe.remove({ userId: req.user._id, _id: req.params.recipe_id }, function (err) {
     if (err)
       res.send(err);
 
-    res.json({ message: 'Recipe removed' });
+    res.res.sendStatus(204);
   });
 };
---
+++ tests/routes.js
@@ -0,0 +1,187 @@
+var assert = require('assert');
+var request = require('supertest');
+var base64 = require('base-64');
+var _ = require('lodash');
+
+describe('routes', function () {
+    var url = "http://localhost:3000";
+
+    // function to generate random usernames
+    var generateRandomString = function () {
+        var randomNumStr = Math.random().toString();
+        return randomNumStr.substr(randomNumStr.indexOf(".") + 1);
+    }
+
+    // a shared user between tests
+    var sharedUser = {
+        username: "common" + generateRandomString(),
+        password: "commonPassword"
+    };
+
+    var sharedUserEncodedCredentials = base64.encode(sharedUser.username + ":" + sharedUser.password);
+
+    // recipe for the shared user. It will be created and then reused accross tests
+    var recipeOfSharedUser = {
+        title: "chushki"
+    };
+
+    before(function (done) {
+        // create the shared user
+        request(url)
+            .post('/users')
+            .send(sharedUser)
+            .end(function (err, res) {
+                if (err) {
+                    throw err;
+                }
+                assert.equal(200, res.status);
+
+                // create the recipe of the shared user
+                request(url)
+                    .post("/recipes")
+                    .set('Authorization', "Basic " + sharedUserEncodedCredentials)
+                    .send(recipeOfSharedUser)
+                    .end(function (err, res) {
+                        if (err) {
+                            throw err;
+                        }
+
+                        recipeOfSharedUser._id = res.body._id;
+                        // now we're ready to run the tests
+                        done();
+                    });
+            });
+    });
+
+    describe('root route', function () {
+        it('should reply with hello message', function (done) {
+            request(url)
+                .get('/')
+                .end(function (err, res) {
+                    assert.equal(200, res.status);
+                    assert.equal('Hello authenticated dude!', res.text);
+                    done();
+                });
+        });
+    });
+
+    describe('users controller', function () {
+        it('should be able to register users', function (done) {
+            // generate random user
+            var credentials = {
+                username: 'user' + generateRandomString(),
+                password: 'password1'
+            }
+
+            // register the user
+            request(url)
+                .post('/users')
+                .send(credentials)
+                .end(function (err, res) {
+                    if (err) {
+                        throw err;
+                    }
+
+                    // check you get status 200 and an access token
+                    assert.equal(200, res.status);
+                    done();
+                });
+        });
+    });
+
+    describe("recipes controller", function () {
+        it('should be able to get recipes', function (done) {
+            request(url)
+                .get("/recipes")
+                .set('Authorization', "Basic " + sharedUserEncodedCredentials)
+                .expect('Content-Type', /json/)
+                .end(function (err, res) {
+                    if (err) {
+                        throw err;
+                    }
+
+                    // make sure you get status 200 + an array of recipes, one of which is the recipeOfSharedUser 
+                    assert.equal(200, res.status);
+                    assert.equal(true, Array.isArray(res.body));
+
+                    var idx = _.findIndex(res.body, function (recipe) {
+                        return recipe._id == recipeOfSharedUser._id;
+                    });
+                    assert.ok(idx != -1);
+                    done();
+                });
+        });
+
+        it('should be able to create and delete recipes', function (done) {
+            var tempRecipe = {
+                title: "temp recipe",
+            }
+
+            request(url)
+                .post("/recipes")
+                .set('Authorization', "Basic " + sharedUserEncodedCredentials)
+                .send(tempRecipe)
+                .end(function (err, res) {
+                    if (err) {
+                        throw err;
+                    }
+
+                    // make sure you get status 200 and the recipe you just created 
+                    // make sure it now has _id property
+                    assert.equal(200, res.status);
+                    assert.ok(res.body._id);
+                    assert.ok(res.body.title);
+
+                    // delete the recipe we just created
+                    request(url)
+                        .del("/recipes/" + res.body._id)
+                        .set('Authorization', "Basic " + sharedUserEncodedCredentials)
+                        .end(function (err, res) {
+                            if (err) {
+                                throw err;
+                            }
+
+                            // make sure we get status 204. nothing else is returned
+                            assert.equal(204, res.status);
+                            done();
+                        });
+                });
+        });
+
+        //     it('should be able to update todos', function (done) {
+        //         // sign in the shared user
+        //         request(url)
+        //             .post('/login')
+        //             .send(sharedUser)
+        //             .end(function (err, res) {
+        //                 var token = res.body.token;
+
+        //                 // get all todos
+        //                 request(url)
+        //                     .get("/api/todos")
+        //                     .set('access_token', token)
+        //                     .end(function (err, res) {
+
+        //                         // find the sharedUserTodo which we know already exists
+        //                         var idx = _.findIndex(res.body, function (todo) {
+        //                             return todo._id == todoOfSharedUser._id;
+        //                         });
+
+        //                         // modify the todo returned from the server
+        //                         var todo = res.body[idx];
+        //                         todo.title = "modified";
+        //                         todo.isDone = true;
+
+        //                         // send the modified todo back to the server
+        //                         request(url)
+        //                             .put("/api/todos/" + todo._id)
+        //                             .set("access_token", token)
+        //                             .end(function (err, res) {
+        //                                 assert.equal(200, res.status);
+        //                                 done();
+        //                             });
+        //                     });
+        //             });
+        //     });
+    });
+});
